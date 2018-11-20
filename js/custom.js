e5ed4998492cea623d5640b7801e7e19a4431427
Ankit Sigdel <mta.ankit@gmail.com>
2018-11-20 09:00:41 +0545

    Search Page 1St Iteration

--- js/custom.js
+++ js/custom.js
@@ -42,11 +42,78 @@ $(function(){
 	// 	}
 	// })
 
+	// Home Arrow Down
 	$('.arrow-down a').click(function(e){
 		e.preventDefault();
 		$('body, html').animate({
 			scrollTop: $(window).height()
 		},500)
+	});
+
+	// Filter Dropdowns
+	$('.spl-filter ul.sf-options > li > a').click(function(e){
+		e.preventDefault();
+		findIfAlreadyOpen= $(this).parent().find('ul').css('display');
+		if(findIfAlreadyOpen == 'block'){
+			$(this).parent().find('ul').stop().slideUp(350);
+			$(this).parent().removeClass('filter-open');
+		}
+		else{
+			$(this).parent().find('ul').stop().slideDown(350);
+			$(this).parent().addClass('filter-open');
+		}
+	});
+
+	// Counter in Store Page
+	// Set counter default to zero
+
+
+	var counter;
+
+	$(".pi-in-dc .pi-btn").click(function(){
+		currentCount = $(this).closest('.pi-in-dc').find('.pi-count>input').val();
+		console.log(currentCount)
+		subtractCase = $(this).hasClass('pi-dc');
+		additionCase = $(this).hasClass('pi-in');
+
+			if(subtractCase){
+				if(currentCount > 0){
+					currentCount--;
+					$(this).closest('.pi-in-dc').find('.pi-count>input').val(currentCount)
+				}
+			}
+			if(additionCase){
+				currentCount++;
+				$(this).closest('.pi-in-dc').find('.pi-count>input').val(currentCount)
+			}
+
+	});
+
+
+	// Store Grid View
+	$('.sg-grid > a').click(function(e){
+		e.preventDefault();
+		ifSingle = $(this).parent().hasClass('sg-single');
+		$('.sg-grid').removeClass('active');
+		if(ifSingle){
+			$('.sg-grid.sg-single').addClass('active');
+			$('.p-listings > .row').addClass('single-line-item');
+			$('.pl-item').each(function(){
+				$(this).children('.wrap-single').wrapAll('<div class="mytable"></div>');
+				$(this).find('.pi-img').wrapAll('<div class="wrapped-img table-cell va-middle"></div>');
+				$(this).find('.pi-title,.pi-desc,.pi-icons,.pi-price-cart').wrapAll('<div class="wrapped-detail table-cell va-middle"></div>');
+			});
+		}
+		else{
+			$('.sg-grid.sg-multiple').addClass('active');
+			$('.p-listings > .row').removeClass('single-line-item');
+			$('.pl-item').each(function(){
+				$(this).children('.wrap-single').unwrap('<div class="mytable"></div>');
+				$(this).find('.pi-img').unwrap('<div class="wrapped-img table-cell va-middle"></div>');
+				$(this).find('.pi-title,.pi-desc,.pi-icons,.pi-price-cart').unwrap('<div class="wrapped-detail table-cell va-middle"></div>');
+			});
+		}
 	})
+
 
 })
